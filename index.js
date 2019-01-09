b8e466a5158c68d3280ec3229061f3d45375b274
Abhi_G <devabhi.code@gmail.com>
2019-01-09 04:01:49 +0000

    Update for multiple account functionality, multiple transactions.

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
 
-  constructor(amount) {
+    // Start every acccount balance as 0 
+    this.balance = 0;
+  }
+
+}
+
+class Transaction {
+  
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+class Deposit extends Transaction {
+  
+//a getter.... only calculates when called.
+  get value() {
+    return this.amount;
+  }
+
+}
+
+
+class Withdrawal extends Transaction {
+  
+//a getter.... only calculates when called.
+  get value() {
+    return -this.amount;
   }
 
 }
@@ -15,15 +44,25 @@ class Withdrawal {
 
 
 
+
+
+
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log(`Starting Balance: ${myAccount.balance}`);
+
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(10.00, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(50.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
